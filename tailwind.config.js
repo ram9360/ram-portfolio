e46852429e52294a90290948f98e3426865cc766
ram9360 <srkram00796@gmail.com>
2024-10-06 10:00:19 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+    },
+    screens:{
+     'sm': '640px',
+
+      'lg': '1024px',
+      
+      'xlg': '1280px',
+    }
+  },
+  plugins: [],
+}
+
